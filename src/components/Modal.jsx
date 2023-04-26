f5ecd7ef1a8ab65642a1ee44090541726a6c8d0a
w45272 <w45272@rams.colostate.edu>
2023-04-25 18:37:17 -0600

    A lot of trial and error

---
+++ src/components/Modal.jsx
@@ -0,0 +1,29 @@
+import { createEffect, createSignal } from "solid-js";
+import "./Modal.css";
+
+export default function Modal(props) {
+  let modalRef;
+  let label1Ref;
+  let label2Ref;
+  
+
+
+  createEffect(() => {
+    modalRef.checked = props.modalState();
+  }, props.modalState());
+
+  return (
+    <>
+      <input ref={modalRef} class="modal-state" id="modal-1" type="checkbox" />
+      <div class="modal">
+        <div class="modal__inner">
+          <h2>Welcome!</h2>
+          <p><img src="https://i.imgur.com/HnrkBwB.gif" alt="" />
+            In order to play you must wager some ETH? Is this okay?
+          </p>
+          <button onClick = {() => {props.setModalState(!props.modalState())}}>I Wanna Play!</button>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/routes/api/GetBalance.js
+++ src/routes/api/GetBalance.js
@@ -3,6 +3,5 @@ import ethers from 'ethers';
 export async function GET() {
     let provider = new ethers.WebSocketProvider("ws://localhost:8545");
     let yeet = await provider.getBalance("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-    console.log(yeet);
     return new Response(yeet);
 }
