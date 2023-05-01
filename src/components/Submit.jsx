f632122cf3980b5a1ab1041626d5543ef9dbb4d3
w45272 <w45272@rams.colostate.edu>
2023-05-01 16:17:06 -0600

    changes

--- src/components/Submit.jsx
+++ src/components/Submit.jsx
@@ -3,7 +3,7 @@ import "./Submit.css";
 export default function Submit(props) {
 
   async function handler(){
-    const response = await fetch('http://localhost:3000/api/Transaction')
+    const response = await fetch('http://localhost:3000/api/tx')
     props.setBalance(await response.text())
     
   }
--- src/routes/api/tx.js
+++ src/routes/api/tx.js
@@ -1,23 +1,17 @@
 import ethers from 'ethers';
-import { abi } from '../../../public/abi'
-import { APIEvent, json } from "solid-start/api";
 
 export async function GET() {
-    console.log("here")
     let provider = new ethers.JsonRpcProvider();
-    console.log("here")
     const NNPrivateKey = "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"
     var NNWallet = new ethers.Wallet(NNPrivateKey, provider);
-    console.log("here")
 
-    tx = {
+    await NNWallet.sendTransaction({
         to: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
-        value: utils.parseEther("1.0")
-    }
-    console.log("here")
-    await NNWallet.sendTransaction(tx)
+        value: ethers.parseEther("1.0")
+    })
 
-    const balance = await NNWallet.getBalance()
-
-    return new Response("bruh");
+    const balance = await provider.getBalance("0x70997970C51812dc3A010C7d01b50e0d17dc79C8")
+    const ret = ethers.formatEther(balance)
+    
+    return new Response(ret);
 }
